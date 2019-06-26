c0a23028d85264f450b6c8fbbf7afb6f91f90671
catXian <yafeile@163.com>
2019-06-26 23:11:09 +0800

    打包发布

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 #encoding:utf-8
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 kw = {
 	"name":"wireshark_ext",
 	"version": "0.1",
 	"description": "Wireshark Format Tool",
-	"long description": "A tool for pretty format packet of Wireshark",
+	"long_description": "A tool for pretty format packet of Wireshark",
 	"author":"yafeile",
 	"author_email":"yafeile@sohu.com",
 	"url":"https://github.com/yafeile/wireshark_ext",
 	"extras_require":{
 	   "six":["six>=1.12.0"]
-	}
+	},
+	"packages": find_packages(),
 }
 setup(**kw)
